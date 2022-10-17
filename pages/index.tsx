955f886be38b35e79e95dbfc45b01ea9cd719ffe
bilguungtn <b.baasansuren@gtn.co.jp>
2022-10-17 09:18:22 +0800

    change

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to hihi
+          Welcome to Cypress
         </h1>
 
         <p className={styles.description}>
